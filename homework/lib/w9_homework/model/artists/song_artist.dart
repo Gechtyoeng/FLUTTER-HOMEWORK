d598f849bc79220c2960364c0ffd4d007553e86f
OengGechty <gechtyoeng@gmail.com>
2026-03-25 15:32:45 +0700

    W9-02- implement artist model

---
+++ homework/lib/w9_homework/model/artists/song_artist.dart
@@ -0,0 +1,8 @@
+class SongArtist {
+  final String id;
+  final String name;
+  final String gener;
+  final Uri imageUrl;
+
+  SongArtist({required this.id, required this.name, required this.gener, required this.imageUrl});
+}
