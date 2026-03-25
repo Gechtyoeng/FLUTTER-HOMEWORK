dccc6eaa5c71995e10d0dbd40e3f400319b559c8
OengGechty <gechtyoeng@gmail.com>
2026-03-25 15:34:13 +0700

    W9-02- Inject repo to main

--- homework/lib/w9_homework/main_dev.dart
+++ homework/lib/w9_homework/main_dev.dart
@@ -1,5 +1,7 @@
 import 'package:provider/provider.dart';
- 
+import 'package:w6_homework/w9_homework/data/repositories/artists/artist_repository.dart';
+import 'package:w6_homework/w9_homework/data/repositories/artists/artist_repository_firebase.dart';
+
 import 'data/repositories/songs/song_repository_firebase.dart';
 import 'main_common.dart';
 import 'data/repositories/settings/app_settings_repository_mock.dart';
@@ -12,7 +14,6 @@ List<InheritedProvider> get devProviders {
   final appSettingsRepository = AppSettingsRepositoryMock();
 
   return [
- 
     // 1 - Inject the song repository
     Provider<SongRepository>(create: (_) => SongRepositoryFirebase()),
 
@@ -20,9 +21,10 @@ List<InheritedProvider> get devProviders {
     ChangeNotifierProvider<PlayerState>(create: (_) => PlayerState()),
 
     // 3 - Inject the  app setting state
-    ChangeNotifierProvider<AppSettingsState>(
-      create: (_) => AppSettingsState(repository: appSettingsRepository),
-    ),
+    ChangeNotifierProvider<AppSettingsState>(create: (_) => AppSettingsState(repository: appSettingsRepository)),
+
+    // 4- Inject the artist repository
+    Provider<ArtistRepository>(create: (_) => ArtistRepositoryFirebase()),
   ];
 }
 
